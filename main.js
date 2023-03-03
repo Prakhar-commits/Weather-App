1aeced9c89d1c7d5bf17f8c44f04413c320282cb
Prakhar <prakharb56@gmail.com>
2023-03-03 19:21:34 +0530

    adding the API

---
+++ main.js
@@ -0,0 +1,12 @@
+const APIKEY = "31a6db7ce0091bfd0c77b3e1e3fa15a3";
+
+const getcurrentweather ={
+    method: "GET",
+    header,
+}
+let headers = new Headers();
+headers.append("apikey",)
+
+document.addEventListener("DOMContentLoaded", ()=>{
+
+})
