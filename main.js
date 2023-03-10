131b485e39749e42d060b4b34946254d6c4afa00
Prakhar <prakharb56@gmail.com>
2023-03-10 07:16:14 +0530

    completing current forecast changes

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 const APIKEY = "31a6db7ce0091bfd0c77b3e1e3fa15a3";
 
-const getcurrentweather ={
-    method: "GET",
-    header,
+const getcurrentweather = async()=>{
+    const city = "DELHI"
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`)
+    return response.json();
 }
-let headers = new Headers();
-headers.append("apikey",)
 
-document.addEventListener("DOMContentLoaded", ()=>{
+// const formattedtemperature = (temp) => `${temp?.tofixed(1)}°`;
+const loadCurrentforecast = ({name , main:{temp , temp_max , temp_min }, weather:[{description}]}) =>{
+    const currentforecastelement = document.querySelector("#current-forecast");
+    currentforecastelement.querySelector(".city").textContent = name;
+    currentforecastelement.querySelector(".temp").textContent = temp;
+    currentforecastelement.querySelector(".description").textContent = description;
+    currentforecastelement.querySelector(".highlowtemp").textContent = `H : ${temp_max} , L : ${temp_min}`
+} 
 
+document.addEventListener("DOMContentLoaded",  async()=>{
+ const currentweather = await getcurrentweather();
+ loadCurrentforecast(currentweather);
 })
