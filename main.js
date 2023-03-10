6bc5e5ae598028df34001b7d8213562df120f02c
Prakhar <prakharb56@gmail.com>
2023-03-11 01:08:08 +0530

    making hourly forecast now

--- main.js
+++ main.js
@@ -6,16 +6,34 @@ const getcurrentweather = async()=>{
     return response.json();
 }
 
-// const formattedtemperature = (temp) => `${temp?.tofixed(1)}°`;
+const getHourlyforecast = async() =>{
+const city = "DELHI"
+const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKEY}`)
+const data = await response.json();
+return data.list.map(forecast => {
+    const { main: {temp , temp_max , temp_min } , dt , dt_txt , weather: {description , icon }} = forecast;
+    return{ temp , temp_max , temp_min, dt , dt_txt , description , icon}
+})
+
+}
+
+const formattedtemperature = (temp) => `${temp?.toFixed(1)}°`;
 const loadCurrentforecast = ({name , main:{temp , temp_max , temp_min }, weather:[{description}]}) =>{
     const currentforecastelement = document.querySelector("#current-forecast");
     currentforecastelement.querySelector(".city").textContent = name;
-    currentforecastelement.querySelector(".temp").textContent = temp;
+    currentforecastelement.querySelector(".temp").textContent = formattedtemperature(temp);
     currentforecastelement.querySelector(".description").textContent = description;
-    currentforecastelement.querySelector(".highlowtemp").textContent = `H : ${temp_max} , L : ${temp_min}`
+    currentforecastelement.querySelector(".highlowtemp").textContent = `H : ${formattedtemperature(temp_max)} , L : ${formattedtemperature(temp_min)}`
 } 
+
+
+const loadHourlyforecast = (hourlyforecast) =>{
+    console.log(hourlyforecast);
+}
+
 
 document.addEventListener("DOMContentLoaded",  async()=>{
  const currentweather = await getcurrentweather();
  loadCurrentforecast(currentweather);
+ const hourlyforecast = await getHourlyforecast(currentweather)
 })
